5c1e28515f175a6b7d4211a755144ff076241539
Harry Sha <harryshahai@gmail.com>
2020-02-24 18:23:01 -0800

    add iou

---
+++ src/metrics/iou.py
@@ -0,0 +1,30 @@
+from .metric import Metric
+
+SMOOTH = 1e-6
+
+
+class IoU(Metric):
+    def __init__(self):
+        super().__init__()
+        self.epoch_acc = 0.0
+        self.running_acc = 0.0
+
+    def formatted(self, computed_val):
+        return f'{self.name}: {100. * computed_val:.2f}%'
+
+    def reset(self):
+        self.running_acc = 0.0
+
+    def update(self, val_dict):
+        output, target = val_dict['output'], val_dict['target']
+        output = output > 0.5
+        accuracy = (((output & (target).bool()).float().sum((1, 2)) + SMOOTH)/((output | target.bool()).float().sum((1, 2)) + SMOOTH)).sum().item()
+        self.epoch_acc += accuracy
+        self.running_acc += accuracy
+        return accuracy
+
+    def get_batch_result(self, log_interval):
+        return self.running_acc / log_interval
+
+    def get_epoch_result(self, num_examples):
+        return self.epoch_acc / num_examples
