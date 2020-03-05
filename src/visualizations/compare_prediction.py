1cede4822ae707ff615f701e71a4129adfd3a111
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-03-05 08:17:05 +0000

    resolve

--- src/visualizations/compare_prediction.py
+++ src/visualizations/compare_prediction.py
@@ -6,8 +6,8 @@ from .viz_utils import save_figure
 def compare_prediction(model, data, target, run_name):
     print("Comparing predictions to actual")
     model.eval()
-    pred = model(data).detach().numpy() > 0.3
-    NUM_EXAMPLES = 4
+    pred = model(data).detach().cpu().numpy() > 0.3
+    NUM_EXAMPLES = 16
     data, pred, target = data.squeeze(), pred.squeeze(), target.squeeze()
     _, axs = plt.subplots(NUM_EXAMPLES, 3, figsize=(10, 20))
     for i in range(NUM_EXAMPLES):
