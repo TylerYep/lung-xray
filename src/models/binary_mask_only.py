a84550f16d248145461e011506c413b791468713
Tyler Yep <tyep@stanford.edu>
2020-03-12 00:17:19 -0700

    fix the indexing using model

--- src/models/binary_mask_only.py
+++ src/models/binary_mask_only.py
@@ -7,24 +7,30 @@ from .binary import Binary
 
 class BinaryMaskOnly(nn.Module):
     """ Neural network """
-    def __init__(self, binary_checkpoint, mask_checkpoint):
+    def __init__(self, binary_checkpoint=None, mask_checkpoint=None):
         super().__init__()
-        binary_path = os.path.join('checkpoints', binary_checkpoint, 'model_best.pth.tar')
-        mask_only_path = os.path.join('checkpoints', mask_checkpoint, 'model_best.pth.tar')
-        bin_model_weights = torch.load(binary_path)#, map_location=torch.device('cpu'))
-        mask_only_model_weights = torch.load(mask_only_path)#, map_location=torch.device('cpu'))
         self.bin_model = Binary()
         self.mask_only_model = UNet()
-        self.bin_model.load_state_dict(bin_model_weights['state_dict'])
-        self.mask_only_model.load_state_dict(mask_only_model_weights['state_dict'])
+        if binary_checkpoint:
+            binary_path = os.path.join('checkpoints', binary_checkpoint, 'model_best.pth.tar')
+            bin_model_weights = torch.load(binary_path)
+            self.bin_model.load_state_dict(bin_model_weights['state_dict'])
+        if mask_checkpoint:
+            mask_only_path = os.path.join('checkpoints', mask_checkpoint, 'model_best.pth.tar')
+            mask_only_model_weights = torch.load(mask_only_path)
+            self.mask_only_model.load_state_dict(mask_only_model_weights['state_dict'])
 
     def forward(self, x):
         """ Forward pass for your feedback prediction network. """
-        out = torch.cat([x] * 3, dim=1)
-        is_binary = self.bin_model(out)
-        if is_binary > 0.5:
-            return torch.zeros(out.shape[1], out.shape[2])
-        return self.mask_only_model(out)
+        is_binary = self.bin_model(x)
+        result = self.mask_only_model(x)
+        indices = torch.nonzero(is_binary < 0.5)
+        result[indices] = torch.zeros(x.shape[1], x.shape[2])
+        return result
 
     def forward_with_activations(self, x):
         pass
+
+
+if __name__ == '__main__':
+    BinaryMaskOnly()(torch.randn((10, 1, 256, 256)))
